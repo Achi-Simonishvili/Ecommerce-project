d9318bbbd3fabc5590bf4558156c3dfa7fe98454
Achi-Simonishvili <a.simonishvili@gmail.com>
2024-06-14 12:26:55 +0400

    update hero images to display on github page

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -8,19 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class HeroComponent implements OnInit {
   images = [
     {
-      url: '/assets/images/store1.jpg',
+      url: '/Ecommerce-project/assets/images/store1.jpg',
       title: 'Effortlessly Blend Comfort & Style!',
       description:
         'Effortlessly blend comfort and style with our Casual & Everyday collection, featuring cozy sweaters, versatile denim, laid-back tees, and relaxed-fit joggers for your everyday adventures',
     },
     {
-      url: '/assets/images/store2.jpg',
+      url: '/Ecommerce-project/assets/images/store2.jpg',
       title: 'Explore Our Diverse Collection!',
       description:
         'Transform your wardrobe with our premium selection of elegant and stylish pieces. Perfect for every occasion, our collection ensures you always look your best.',
     },
     {
-      url: '/assets/images/store3.jpg',
+      url: '/Ecommerce-project/assets/images/store3.jpg',
       title: 'Discover The Allure Of Fashion Reinvented!',
       description:
         'Dive into a world of style with our latest collection! Shop now and redefine your wardrobe narrative!',
